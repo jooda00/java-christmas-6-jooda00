108185ff3cb99fbdce46a0cfc558af178d3c5ef7
jooda00 <dragon2000a@gmail.com>
2023-11-15 02:08:34 +0900

    refactor: EventTest에서 EventDiscountTest로 할인 테스트 옮김

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -4,8 +4,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Event {
-    private static final int CHRISTMAS_DAY = 25;
-    private static final int DISCOUNT_AMOUNT = 2023;
     private final int date;
     private final Map<String, Integer> menus = new LinkedHashMap<>();
     private int total = 0;
@@ -22,33 +20,6 @@ public class Event {
         for (Map.Entry<String, Integer> pair : menus.entrySet()) {
             this.total += EventMenu.calculateTotalPrice(pair.getKey(), pair.getValue());
         }
-    }
-
-    public int executeChristmasDiscount() {
-        int discount = 1000;
-        if (this.date <= CHRISTMAS_DAY) {
-            discount += 100 * (this.date - 1);
-            return discount;
-        }
-        return 0;
-    }
-
-    public int executeWeekdayDiscount() {
-        int discount = 0;
-        if (EventDay.isWeekDay(this.date)) {
-            discount = EventMenu.calculateDessertCount(this.getMenus());
-            return discount * DISCOUNT_AMOUNT;
-        }
-        return discount;
-    }
-
-    public int executeWeekendDiscount() {
-        int discount = 0;
-        if (EventDay.isWeekend(this.date)) {
-            discount = EventMenu.calculateMainCount(this.getMenus());
-            return discount * DISCOUNT_AMOUNT;
-        }
-        return discount;
     }
 
     public int getDate() {
---
+++ src/test/java/christmas/model/EventDiscountTest.java
@@ -0,0 +1,46 @@
+package christmas.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventDiscountTest {
+    private Event event;
+    private EventDiscount eventDiscount;
+
+    @BeforeEach
+    void setUp() {
+        event = new Event(25);
+        eventDiscount = new EventDiscount();
+        event.addMenu("티본스테이크", 3);
+        event.addMenu("초코케이크", 2);
+        event.addMenu("레드와인", 1);
+    }
+
+    @Test
+    void 크리스마스_디데이_할인() {
+        int discount = 3400;
+
+        assertThat(discount).isEqualTo(eventDiscount.executeChristmasDiscount(event));
+    }
+
+    @Test
+    void 평일_할인() {
+        int discount = 4046;
+
+        assertThat(discount).isEqualTo(eventDiscount.executeWeekdayDiscount(event));
+    }
+
+    @Test
+    void 주말_할인() {
+        event = new Event(23);
+        eventDiscount = new EventDiscount();
+        event.addMenu("티본스테이크", 3);
+        event.addMenu("초코케이크", 2);
+        event.addMenu("레드와인", 1);
+        int discount = 6069;
+
+        assertThat(discount).isEqualTo(eventDiscount.executeWeekendDiscount(event));
+    }
+}
